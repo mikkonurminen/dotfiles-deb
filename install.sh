459c6dd1dc089032f0d2b53bffca3f30c4ce698b
mikkonurminen <mmnurm@gmail.com>
2023-06-20 23:33:16 +0300

    del

--- install.sh
+++ install.sh
@@ -19,8 +19,5 @@ sudo mv nvim.appimage /usr/local/bin/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-echo 'Install npm packages'
-source ~/dotfiles-deb/npm.sh
-
 echo "Symlink dotfiles"
 source ~/dotfiles-deb/symlink.sh
