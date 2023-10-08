2336040e1fae3ff704d5677043eae5158c96d40a
mikkonurminen <mmnurm@gmail.com>
2023-10-08 22:38:14 +0300

    jotain

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,38 +1,38 @@
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-    --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    --vim.keymap.set('n', '<space>wl', function()
-    --print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    --end, opts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    --vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-    vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, {})
-    --vim.keymap.set('n', '<space>f', function()
-    -- vim.lsp.buf.format { async = true }
-    --end, opts)
-  end,
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+		--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		--vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		--vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		--vim.keymap.set('n', '<space>wl', function()
+		--print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		--end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		--vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+		vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, {})
+		--vim.keymap.set('n', '<space>f', function()
+		-- vim.lsp.buf.format { async = true }
+		--end, opts)
+	end,
 })
 
 -- Setup language servers.
@@ -41,32 +41,36 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = require("cmp_nvim_lsp").on_attach
 
 lspconfig.lua_ls.setup({
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 lspconfig.tsserver.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 lspconfig.eslint.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 
 -- Enable (broadcasting) snippet capability for completion
 --local cap_snippet = vim.lsp.protocol.make_client_capabilities()
 --cap_snippet.textDocument.completion.completionItem.snippetSupport = true
 lspconfig.cssls.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 lspconfig.html.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 lspconfig.pyright.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+lspconfig.bashls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
